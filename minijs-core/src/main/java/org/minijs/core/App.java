af821c10e755cf7d242a0bbb5cd4a8188e31575b
Frank Du <qixing.du@gmail.com>
2013-09-10 08:08:38 -0700

    Add the parser module. Add helloworld-like java files to each modules

---
+++ minijs-core/src/main/java/org/minijs/core/App.java
@@ -0,0 +1,13 @@
+package org.minijs.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
